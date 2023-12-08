0178739ddf1bbce286af7a155863c36e197e85e4
CristhianDavid <emmacris04@gmail.com>
2023-12-08 14:25:50 -0500

    Agregando estudios JS

--- JS/JS/intro-dom/Appi 1/main.js
+++ JS/JS/intro-dom/Appi 1/main.js
@@ -1,6 +1,27 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+const render = () => {
+    const todoList = document.getElementById('todo-list');
+    const todosTemplate = todos.map (t => '<li>' + t + '</li>');
+    todoList.innerHTML = todosTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li')
+    elementos.forEach((elemento, i) => {
+        elemento.addEventListener('click', () => {
+            elemento.parentNode.removeChild(elemento)
+            todos.splice(i, 1)
+            actualizarTodo(todos);
+            render();            
+        })          
+    })
+}
+
+const actualizarTodo = (todos) => {
+    const todoString = JSON.stringify(todos);
+    localStorage.setItem('todos', todoString)
+}
 
 window.onload = () => {
+    render();
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) => {
         e.preventDefault();
@@ -8,18 +29,19 @@ window.onload = () => {
         const todoText = todo.value;
         todo.value = '';
         todos.push(todoText);
-        const todoList = document.getElementById('todo-list');
-        const todosTemplate = todos.map (t => '<li>' + t + '</li>');
-            todoList.innerHTML = todosTemplate.join('');
-        // todoList.innerHTML = '';
-        // for (let i = 0; i < todos.length; i++) {
-        //     todoList.innerHTML += '<li>' + todos[i] + '</li>';
-        // }
-        console.log(todosTemplate);
+        actualizarTodo(todos);
+        render();
+       
     }        
 }
 
 //MAP nos permite iterar los arreglos sin necesidad de que le indiquemos los indices ni las operaciones
+
+
+
+
+
+
 
 
 
